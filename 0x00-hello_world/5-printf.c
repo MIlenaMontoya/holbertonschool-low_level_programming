461cf9a2601a1b0acca63ca4b0c2cbe55c3bb437
MIlenaMontoya <1852@holbertonschool.com>
2020-06-11 04:02:06 +0000

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,2 @@
-#include <stdio.h>
-int main(void) /*found for function main*/
-{
-printf("with proper grammar, but the outcome is a piece of art,");
-()return 0;
-}
+#include <stdio.h> /*funtion main*/
+int main(void) { printf("with proper grammar, but the outcome is a piece of art,"); ()return 0; }
