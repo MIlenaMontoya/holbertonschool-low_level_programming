3e0e95576eea777a7cc0fe7818bf954e11ccc56e
MIlenaMontoya <1852@holbertonschool.com>
2020-06-11 03:20:04 +0000

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,2 @@
+#include <stdio.h> /*primera linea obligada que incluye libreria stdio */
+int main(void) { puts("\"Programming is like building a multilingual puzzle"); return 0; } /*funcion principal */
