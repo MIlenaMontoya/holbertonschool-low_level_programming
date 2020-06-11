b6d15ef7c187b3ae23191e1f796c5e422fd00d3b
MIlenaMontoya <1852@holbertonschool.com>
2020-06-11 04:51:30 +0000

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(void)
+{
+printf("Size of a char: %i bytes(s)\n", sizeof(char));
+printf("Size of an int: %i bytes(s)\n", sizeof(int));
+printf("Size of a long int: %i bytes(s)\n", sizeof(int));
+printf("Size of a long long int: %i bytes(s)\n", sizeof(long));
+printf("Size of a float: %i bytes(s)\n", sizeof(float));
+()return 0;
+}
