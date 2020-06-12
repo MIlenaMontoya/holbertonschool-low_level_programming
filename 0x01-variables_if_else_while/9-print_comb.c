1760519bcec0aeab70ec77704bfdac11ca05448b
MIlenaMontoya <1852@holbertonschool.com>
2020-06-12 22:41:09 +0000

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - num, combi
+ *
+ * Return: 0 always runs
+ */
+
+int main(void)
+{
+	int m ='0';
+
+	while (m <= '9')
+		{
+	putchar (m);
+if (m != '9')
+		{
+putchar (',');
+putchar (' ');
+}
+else 
+{
+putchar ('\n');
+}
+m++;
+	}
+	return (0);
+}
