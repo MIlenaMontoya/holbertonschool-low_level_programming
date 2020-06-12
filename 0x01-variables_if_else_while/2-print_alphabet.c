8c4fdc7e6768fc69a4e0d33b4e7351cff2b3109b
MIlenaMontoya <1852@holbertonschool.com>
2020-06-12 03:11:06 +0000

    2-print_alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h> 
+/**
+ * main - abc
+ *
+ * Return: 0 always runs
+ */  
+
+int main() 
+{ 
+  
+   
+char ch = 'a'; 
+  
+   
+for (ch = 'a'; ch <= 'z'; ch++) 
+putchar(ch "\n"); 
+ 
+return (0); 
+} 
