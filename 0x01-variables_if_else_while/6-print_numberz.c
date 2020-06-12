474ebcdb0d88fe5f7e147596dbc1efa06b596ccf
MIlenaMontoya <1852@holbertonschool.com>
2020-06-12 19:03:42 +0000

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - abc
+ *
+ * Return: 0 always runs
+ */
+
+int main(void)
+{
+	int m = 48;
+
+	while (m < 58)
+		{
+	putchar (m);
+	m++;
+		}
+	putchar ('\n');
+	return (0);
+}
