7ac479a58e3ca95f0607a20466826089fb6dd803
MIlenaMontoya <1852@holbertonschool.com>
2020-06-12 21:20:14 +0000

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - abc
+ *
+ * Return: 0 always runs
+ */
+
+int main(void)
+{
+	char m = 48;
+	char i = 97;
+
+	while (m < 58)
+		{
+	putchar (m);
+	m++;
+		}
+	while (i < 103)
+		{
+	putchar (i);
+	i++;
+		}
+	putchar ('\n');
+
+	return (0);
+}
