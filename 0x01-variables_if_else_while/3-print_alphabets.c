ba47c038cf5b82eedab8f54c9ffe41114f76d7c3
MIlenaMontoya <1852@holbertonschool.com>
2020-06-12 18:33:05 +0000

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - abc
+ *
+ * Return: 0 always runs
+ */
+
+int main(void)
+{
+	char m = 97;
+	char i = 65;
+
+	while (m < 123)
+		{
+		putchar (m);
+		m++;
+		}
+		while (i < 91)
+		{
+		putchar (i);
+		i++;
+		}
+	putchar ('\n');
+
+return (0);
+}
