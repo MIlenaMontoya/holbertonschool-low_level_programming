987099b26c57ec261f62afb2164953acee2d4a1c
MIlenaMontoya <1852@holbertonschool.com>
2020-06-15 16:04:31 +0000

    0x02-functions_nested_loops

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - abc
+ *
+ * Return: 0 always runs
+ */
+
+int main(void)
+{
+	char m = 'a';
+	while (m <= 'z')
+		{
+	if (m != 'e' && m != 'q')
+	putchar (m);
+	m++;
+	}
+	putchar ('\n');
+
+	return (0);
+}
