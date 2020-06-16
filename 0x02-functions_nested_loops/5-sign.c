28a4382211f078f75e118a7ce6b70763fd7ca159
MIlenaMontoya <1852@holbertonschool.com>
2020-06-16 21:22:49 +0000

    5-signs.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+
+/**
+ * print_sign - signos
+ * @n: input function
+ *
+ * Description: the sign of a number
+ *
+ * Return: 1 if greater than 0, 0 if 0, -1 if -
+ */
+
+int print_sign(int n)
+
+{
+	if (n > 0)
+		{
+		_putchar ('+');
+		return (1);
+		}
+	else if (n == 0)
+		{
+		_putchar ('0');
+		return (0);
+		}
+	else
+	_putchar ('-');
+	return (-1);
+}
