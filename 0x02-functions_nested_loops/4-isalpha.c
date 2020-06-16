ae6d65597c1992e1d221e281fd104d1030ebc4cf
MIlenaMontoya <1852@holbertonschool.com>
2020-06-16 20:53:33 +0000

    4-isalpha.c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ * _isalpha - lowercase
+ * @c: letter lowercase or uppercase
+ *
+ * Description: lowercase or uppercase
+ *
+ * Return: 1 for lowercase or 0 not
+ */
+
+int _isalpha(int c)
+
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+		{
+			return (1);
+		}
+	else
+		{
+			return (0);
+		}
+}
