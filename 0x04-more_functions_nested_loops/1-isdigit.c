02fa48a8fa9adf9553e43cd760d6f170d94824ea
MIlenaMontoya <1852@holbertonschool.com>
2020-06-18 17:25:54 +0000

    1-isdigit.c

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,15 +1,17 @@
 #include "holberton.h"
 
 /**
- * _isupper - function checks for uppercase char
- * @c: input
- * Return: 1 if c is uppercase, 0 otherwise
+ * _isdigit - lowercase
+ * @c: checks for a digit
+ *
+ * Description: function that checks for a digit
+ * Return: 1 for lowercase or 0 not
  */
 
-int _isupper(int c)
+int _isdigit(int c)
 
 {
-	if (c >= 'A' && c <= 'Z')
+	if (c >= '0' && c <= '9')
 		return (1);
 	else
 		return (0);
