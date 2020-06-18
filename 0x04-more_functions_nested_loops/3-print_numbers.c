72b852102802c6f0d25fecb10744334f0f610089
MIlenaMontoya <1852@holbertonschool.com>
2020-06-18 20:44:32 +0000

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * print_numbers - print num de 0 at 9
+ *
+ *
+ * Return: 0
+ */
+
+void print_numbers(void)
+{
+	char a;
+
+for (a = '0'; a <= '9'; a++)
+	{
+	_putchar(a);
+	}	
+        _putchar('\n');
+	
+}
