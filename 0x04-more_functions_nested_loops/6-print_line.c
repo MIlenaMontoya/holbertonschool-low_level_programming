f423f32c609960c511bab18d19ab14175bde9a2e
MIlenaMontoya <1852@holbertonschool.com>
2020-06-19 03:55:11 +0000

    6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+ * print_line - print lines
+ *@n: int input
+ *
+ * Return: 0
+ */
+
+void print_line(int n)
+
+{
+	int a;
+for (a = 0; a < n; a++)
+{
+_putchar ('_');
+}
+{
+_putchar ('\n');
+}
+
+}
