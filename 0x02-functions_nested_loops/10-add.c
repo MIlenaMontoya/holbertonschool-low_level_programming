ef7ed73d433214501aef33ac10dc87e90977c002
MIlenaMontoya <1852@holbertonschool.com>
2020-06-19 04:43:54 +0000

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ * add - adiccion two integers
+ * @a: first int
+ * @b: second int
+ * Description: function adiccion two integers
+ * Return: (a + b)
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
