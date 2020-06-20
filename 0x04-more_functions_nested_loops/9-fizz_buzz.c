94c937abdb2369d5664a11fd392bf4db6cf1f0cf
MIlenaMontoya <1852@holbertonschool.com>
2020-06-20 23:00:18 +0000

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -10,25 +10,18 @@
 int main(void)
 {
 
-	int n;
+	int num;
 
-for (n = 1; n <= 100; n++)
-
-if (n % 15 == 0)
+for (num = 1; num <= 100; num++)
 	{
+if (num % 15 == 0)
 	printf("FizzBuzz ");
-	}
-else if (n % 5 == 0)
-	{
+else if (num % 5 == 0)
 	printf("Buzz ");
-	}
-else if (n % 3 == 0)
-	{
-	printf("Fizz ");
-	}
+else if (num % 3 == 0)
+printf("Fizz ");
 else
-	{
-	printf("%d ", n);
+printf("%i ", num);
 }
 printf("\n");
 return (0);
