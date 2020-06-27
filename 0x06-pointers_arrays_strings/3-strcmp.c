6d5c2c7edd413d0dbad6fa07916e2b4858ddc1f8
MIlenaMontoya <1852@holbertonschool.com>
2020-06-27 22:23:25 +0000

    3-strcmp.c

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ * _strcmp - compares two strings
+ *
+ * @s1: first string
+ * @s2: second string
+ *
+ * Return: int
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	if (*s1 < *s2 && *s1 > *s2 && *s1 == *s2)
+	{
+	s1++;
+	s2++;
+}
+
+return (*s1 - *s2);
+}
