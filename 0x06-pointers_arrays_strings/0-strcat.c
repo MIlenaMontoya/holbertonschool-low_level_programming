f4895452704bb14e36ce1bd369e95d02267a872f
MIlenaMontoya <1852@holbertonschool.com>
2020-06-27 19:30:39 +0000

    0-strcat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+
+/**
+ * _strcat - concat two strings
+ *
+ * @dest: first string
+ * @src: second string
+ * Return: dest
+ */
+
+char *_strcat(char *dest, char *src)
+{
+int cont1, cont2;
+for (cont1 = 0; dest[cont1] != '\0'; cont1++)
+	{
+}
+
+for (cont2 = 0; src[cont2] != '\0'; cont2++, cont1++)
+{
+dest[cont1] = src[cont2];
+	}
+cont1++;
+dest[cont1] = '\0';
+	return (dest);
+
+}
