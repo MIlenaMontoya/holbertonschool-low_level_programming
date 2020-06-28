12b441e7eab4f337e657a2abb3ca3a20995b3ec0
MIlenaMontoya <1852@holbertonschool.com>
2020-06-28 04:29:13 +0000

    3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -10,12 +10,13 @@
  */
 
 int _strcmp(char *s1, char *s2)
-{
-	if (*s1 < *s2 && *s1 > *s2 && *s1 == *s2 && s2 != '\0')
-	{
-	s1++;
-	s2++;
-}
 
-return (*s1 - *s2);
+{
+int cont;
+
+for (cont = 0; s1[cont] != '\0' && s2[cont] != '\0'; cont++)
+
+if (s1[cont] != s2[cont])
+return (s1[cont] - s2[cont]);
+return (0);
 }
