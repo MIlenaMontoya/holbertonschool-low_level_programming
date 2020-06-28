5d189c26aa2983177f2d4f50f5d40d6fd70a72d5
MIlenaMontoya <1852@holbertonschool.com>
2020-06-28 01:24:34 +0000

    4-rev_array.c

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+ * reverse_array - reverse array
+ *
+ * @n: Elements array
+ * @a: array
+ *
+ * Return: int
+ */
+
+void reverse_array(int *a, int n)
+{
+	int aux, i;
+
+	for (i = n - 1; i >= n / 2; i--)
+	{
+		aux = a[n - 1 - i];
+	        a[n - 1 - i] = a[i];
+		a[i] = aux; 
+}
+}
