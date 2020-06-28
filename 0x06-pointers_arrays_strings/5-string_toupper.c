b39ec5e9403f60bb48d035bc5221f5024d6fe6e5
MIlenaMontoya <1852@holbertonschool.com>
2020-06-28 03:02:49 +0000

    5-string_toupper.c

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -3,18 +3,17 @@
 /**
  * string_toupper - replace letters
  * @a: string converted
- * 
+ *
  * Return: char
  */
 char *string_toupper(char *a)
 {
 	int cont;
-
-	for (cont = 0; a[cont] != '\0'; cont++)
-		{
-			if (a[cont] >= 'a' && a[cont] <= 'z')
-				a[cont] -= 32;
+for (cont = 0; a[cont] != '\0'; cont++)
+	{
+if (a[cont] >= 'a' && a[cont] <= 'z')
+	a[cont] -= 32;
 }
-	return (a);
+return (a);
 
 }
