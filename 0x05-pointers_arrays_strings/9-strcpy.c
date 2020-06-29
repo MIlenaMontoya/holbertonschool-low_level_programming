668d465df8a0983925b92962e868143d5144a91a
MIlenaMontoya <1852@holbertonschool.com>
2020-06-29 04:06:57 +0000

    strcopy

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -13,6 +13,7 @@ char *_strcpy(char *dest, char *src)
 	int cont;
 
 	for (cont = 0; src[cont] != 0; cont++)
-		dest[cont] = src[cont];
+	dest[cont] = src[cont];
+	dest[cont] = 0;
 	return (dest);
 }
