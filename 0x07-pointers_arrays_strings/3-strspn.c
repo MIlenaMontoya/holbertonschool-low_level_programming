54a1435cdf045c8a46bbe983ad0e6e59a923ec95
MIlenaMontoya <1852@holbertonschool.com>
2020-06-29 20:28:12 +0000

    strspn3

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -4,23 +4,20 @@
  * _strspn -  memory with a constant bytes
  * @s: pointer
  * @accept: char byte
- * 
+ *
  * Return: num bytes
  */
 
 unsigned int _strspn(char *s, char *accept)
 {
-	unsigned int cont;
-	int i, j;
+	unsigned int i, j, cont = 0;
 
-	for (i = 0; s[i] != '\0'; i++)
+for (i = 0; s[i] != '\0'; i++)
 	{
-		if (cont != i)
-			break;
-	}
-	for (j = 0; accept[j] != '\0'; j++)
-	{
-	if (s[i] == accept[j])
+if (cont != i)
+break;
+for (j = 0; accept[j] != '\0'; j++)
+if (s[i] == accept[j])
 cont++;
 	}
 	return (cont);
