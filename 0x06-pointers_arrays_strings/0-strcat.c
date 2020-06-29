2867e601b801232883304808f3555167c8b30f42
MIlenaMontoya <1852@holbertonschool.com>
2020-06-29 04:22:32 +0000

    strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -11,16 +11,15 @@
 char *_strcat(char *dest, char *src)
 {
 int cont1, cont2;
+
 for (cont1 = 0; dest[cont1] != '\0'; cont1++)
 	{
-	}
-
+}
 for (cont2 = 0; src[cont2] != '\0'; cont2++, cont1++)
-{
+	{
 dest[cont1] = src[cont2];
 	}
 cont1++;
 dest[cont1] = '\0';
-	return (dest);
-	
+return (dest);
 }
