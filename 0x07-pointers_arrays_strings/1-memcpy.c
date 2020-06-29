45e06775ba73fdfbfc07812923adc198b5ac164f
MIlenaMontoya <1852@holbertonschool.com>
2020-06-29 17:54:53 +0000

    memcopy

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * _memset -  memory with a constant bytes
+ * @dest: pointer
+ * @src: char byte
+ * @n: int bytes de memoria
+ * Return: pointer dest
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int cont;
+
+	for (cont = 0; cont < n; cont++)
+		dest[cont] = src[cont];
+	return (dest);
+
+}
