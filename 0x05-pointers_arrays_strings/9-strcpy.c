6694855c50ff43191647205691911df18a5d90bc
MIlenaMontoya <1852@holbertonschool.com>
2020-06-29 04:00:12 +0000

    strcopy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ * _strcpy - copy string
+ *
+ * @dest: first string
+ * @src: second string
+ * Return: dest
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int cont;
+
+	for (cont = 0; src[cont] != '\0'; cont++)
+		dest[cont] = src[cont];
+	return (dest);
+}
