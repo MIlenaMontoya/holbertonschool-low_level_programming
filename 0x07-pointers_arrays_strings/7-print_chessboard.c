3dc1835efbdf13af0810ec9a100a6a070e6f8976
MIlenaMontoya <1852@holbertonschool.com>
2020-06-29 23:39:05 +0000

    chessboard

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/**
+ * print_chessboard - prints the chessboard
+ *
+ * @a: name array
+ *
+ * Return: void
+ */
+
+void print_chessboard(char (*a)[8])
+
+{
+	int row, col;
+
+	for (row = 0; row < 8; row++)
+		{
+	for (col = 0; col < 8; col++)
+		{
+		_putchar (a[row][col]);
+		}
+	_putchar('\n');
+		}
+}
