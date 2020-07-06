f47807984476808bf659acc35cb1b713bafbda02
MIlenaMontoya <1852@holbertonschool.com>
2020-07-06 23:44:58 +0000

    add

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -11,21 +11,21 @@
 
 int main(int argc, char *argv[])
 {
-  int num, digit;
-    int sum = 0;
+int num, digit;
+int sum = 0;
 
-  for (num = 1; num < argc; num++)
-   {
-     for (digit = 0; argv[num][digit]; digit++)
-	{
-  if (argv[num][digit] < '0' || argv[num][digit] > '9')
-	    {
-      printf("Error\n");
-      return (1);
-	    }
-	}
-      sum = sum + atoi(argv[num]);
-    }
+for (num = 1; num < argc; num++)
+{
+for (digit = 0; argv[num][digit]; digit++)
+{
+if (argv[num][digit] < '0' || argv[num][digit] > '9')
+{
+printf("Error\n");
+return (1);
+}
+}
+sum = sum + atoi(argv[num]);
+}
 printf("%d\n", sum);
 return (0);
 }
