7008a82b02e261949cf39bf867c49a155daa1df9
MIlenaMontoya <1852@holbertonschool.com>
2020-07-06 21:10:40 +0000

    mul

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -11,13 +11,13 @@
 
 int main(int argc, char *argv[])
 {
-
-	if(argc <= 3)
-		{
-			printf("Error\n");
-			return (1);
-		}
-	
-	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+int mul = 0;
+if (argc != 3)
+{
+printf("Error\n");
+return (1);
+	}
+mul = atoi(argv[1]) * atoi(argv[2]);
+	printf("%d\n", mul);
 	return (0);
 }
