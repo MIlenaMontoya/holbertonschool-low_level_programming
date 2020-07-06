a419a82dcb1794366ac9057f41a09ff7649d95ba
MIlenaMontoya <1852@holbertonschool.com>
2020-07-06 18:41:14 +0000

    fff

--- 0x09-static_libraries/5-strstr.c
+++ 0x09-static_libraries/5-strstr.c
@@ -1,14 +1,14 @@
 #include "holberton.h"
 
 /**
- * _strpbrk - locates a character in a string.
+ * _strstr - locates a character in a string.
  * @s: char byte
  * @accept: string
  *
  * Return: pointer a char o null
  */
 
-char *_strpbrk(char *s, char *accept)
+char *_strstr(char *haystack, char *needle)
 
 {
 	unsigned int i, cont;
