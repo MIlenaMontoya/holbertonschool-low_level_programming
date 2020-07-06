c3de08b09746a6dd818b4ee04bf67c86baecbbcd
MIlenaMontoya <1852@holbertonschool.com>
2020-07-06 20:01:38 +0000

    lebreria

--- 0x09-static_libraries/5-strstr.c
+++ 0x09-static_libraries/5-strstr.c
@@ -1,9 +1,9 @@
 #include "holberton.h"
 
 /**
- * _strstr - locates a character in a string.
- * @s: char byte
- * @accept: string
+ * _strstr - locates a character in a string
+ * @haystack: char
+ * @needle: char
  *
  * Return: pointer a char o null
  */
@@ -11,15 +11,5 @@
 char *_strstr(char *haystack, char *needle)
 
 {
-	unsigned int i, cont;
-
-	for (i = 0; s[i] != '\0'; i++)
-		{
-			for (cont = 0; accept[cont] != '\0'; cont++)
-				{
-					if (s[i] == accept[cont])
-						return (s + i);
-				}
-		}
-	return (0);
+return (0);
 }
