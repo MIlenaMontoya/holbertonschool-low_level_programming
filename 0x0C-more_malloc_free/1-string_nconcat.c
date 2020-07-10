22f96847df135cffe1d80285714c3a118a58c061
MIlenaMontoya <1852@holbertonschool.com>
2020-07-10 01:03:05 +0000

    more malloc

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -19,8 +19,10 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		s1 = str;
 	if (s2 == NULL)
 		s2 = str;
-	for (a = 0; s1[a] != '\0'; a++)
-		;
+for (a = 0; s1[a] != '\0'; a++)
+{
+
+}
 	ptr = malloc((a + (n * sizeof(*s2) + 1) * sizeof(*ptr)));
 	if (ptr == NULL)
 		return (NULL);
