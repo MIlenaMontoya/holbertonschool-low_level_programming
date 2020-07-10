0089086f3d35fa15f12b0c421be50cee980d5a3e
MIlenaMontoya <1852@holbertonschool.com>
2020-07-10 00:03:29 +0000

    more malloc

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -12,20 +12,18 @@
 int *array_range(int min, int max)
 
 {
-void *ptr;
+int *ptr;
 int a;
+
 if (min > max)
 return (NULL);
 
 	ptr = malloc(((max - min) + 1) * sizeof(int));
 	if (ptr == NULL)
 		return (NULL);
-             
-for (a = 0; a < ((min - max) + 1); a++)
-       {
-
-       } 
-       
-      
+for (a = 0; min > max; a++)
+{
+ptr[a] = 0;
+}
 return (ptr);
 }
