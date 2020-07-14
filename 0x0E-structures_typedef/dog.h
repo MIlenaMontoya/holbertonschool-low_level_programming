602ba9ca8c30dac6b1f21e29bede4421977a17e9
MIlenaMontoya <1852@holbertonschool.com>
2020-07-14 00:14:56 +0000

    struct

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,23 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+* struct dog - struct
+* @name: name
+* @age: age
+* @owner: owner
+*
+*/
+
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+
+
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+
+#endif
