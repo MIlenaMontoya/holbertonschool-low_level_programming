688c24fcf9f58e9a81c579cd2c6a8c8abfdf8ead
MIlenaMontoya <1852@holbertonschool.com>
2020-07-14 03:24:10 +0000

    struct

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,19 @@
+#include "dog.h"
+
+/**
+ * init_dog - dog structure
+ * @d: dog
+ * @name: name
+ * @age: age
+ * @owner: owner
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+
+if (d != '\0')
+
+d->name = name;
+d->age = age;
+d->owner = owner;
+}
