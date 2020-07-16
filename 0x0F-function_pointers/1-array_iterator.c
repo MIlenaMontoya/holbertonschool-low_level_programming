1bbd69acd09830b6d1efaa053a8c1ada27da10ff
MIlenaMontoya <1852@holbertonschool.com>
2020-07-16 02:07:52 +0000

    function_pointer

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -10,12 +10,11 @@
  * Return: void
  */
 
-        void array_iterator(int *array, size_t size, void (*action)(int))
+void array_iterator(int *array, size_t size, void (*action)(int))
 {
-       unsigned int a;
-
-        for (a = 0; a < size; a++)
+unsigned int a;
+for (a = 0; a < size; a++)
 {
-        action(array[a]);
+action(array[a]);
 }
 }
