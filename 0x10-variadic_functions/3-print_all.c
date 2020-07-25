b27e51c258f1e71ef8fb3a9f274fa89d4a1719c6
MIlenaMontoya <1852@holbertonschool.com>
2020-07-25 16:16:50 +0000

    funton variadic print all 💎

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,91 @@
+#include <stdio.h>
+#include "variadic_functions.h"
+
+/**
+ * print_char - print a character
+ * @a: char
+ *
+ * Return: 0
+ */
+void print_char(va_list a)
+{
+	printf("%c", va_arg(a, int));
+}
+
+/**
+ * print_string - print a string
+ * @s: string
+ *
+ * Return: 0
+ */
+void print_string(va_list s)
+{
+	char *str = va_arg(s, char *);
+
+	if (str == NULL)
+		str = "(nil)";
+	printf("%s", str);
+}
+
+/**
+ * print_int - print int
+ * @num: int
+ *
+ * Return: 0
+ */
+void print_int(va_list num)
+{
+	printf("%d", va_arg(num, int));
+}
+
+/**
+ * print_float - prints float
+ * @f: float
+ *
+ * Return: 0
+ */
+void print_float(va_list f)
+{
+	printf("%f", va_arg(f, double));
+}
+
+/**
+ * print_all - prints anything
+ * @format: list of argument passed at function
+ *
+ * Return: 0
+ */
+void print_all(const char * const format, ...)
+{
+	int i, j;
+	print_t p[] = {
+		{"c", print_char},
+		{"s", print_string},
+		{"i", print_int},
+		{"f", print_float},
+		{NULL, NULL}
+	};
+	va_list list_print;
+	char *separator = "";
+
+	va_start(list_print, format);
+	i = 0;
+	while (format && format[i])
+	{
+		j = 0;
+		while (p[j].t != NULL)
+		{
+			if (*(p[j].t) == format[i])
+			{
+				printf("%s", separator);
+				p[j].f(list_print);
+				separator = ", ";
+				break;
+			}
+			j++;
+		}
+		i++;
+	}
+	va_end(list_print);
+	printf("\n");
+}
