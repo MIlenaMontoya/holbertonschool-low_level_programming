42f9b2dd63a88a94af3332ea5dd1dc01a4816321
MIlenaMontoya <1852@holbertonschool.com>
2020-07-29 02:05:09 +0000

    check betty description of the function 🤦🏼‍♀️

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * add_nodeint - adds a new node at the beginning of a listint_t list.
+ * add_nodeint_end - adds a new node at the end of a listint_t list.
  * @head: douuble pointer to the list
  * @n: integer to add to the list
  *
@@ -9,25 +9,25 @@
  */
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
-	listint_t *new_node, *aux;
+listint_t *new_node, *aux;
 
-	if (head == NULL)
-		return (NULL);
-	new_node = malloc(sizeof(listint_t));
-	if (new_node == NULL)
-		return (NULL);
-	new_node->n = n;
-	new_node->next = NULL;
-        if (*head == NULL)
-        {
+if (head == NULL)
+return (NULL);
+new_node = malloc(sizeof(listint_t));
+if (new_node == NULL)
+return (NULL);
+new_node->n = n;
+new_node->next = NULL;
+if (*head == NULL)
+{
 
-	*head = new_node;
-	return (new_node);
+*head = new_node;
+return (new_node);
 }
 aux = *head;
 for (; aux->next != NULL;)
 {
-        aux = aux->next;
+aux = aux->next;
 }
 aux->next = new_node;
 return (new_node);
