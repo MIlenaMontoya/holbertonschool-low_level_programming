5b9b4fac9c6b1ee90c5f9c41d8bb8c8394db6b81
MIlenaMontoya <1852@holbertonschool.com>
2020-08-02 00:11:11 +0000

    changes more

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -11,15 +11,13 @@ unsigned int binary_to_uint(const char *b)
 int i;
 unsigned int number = 0;
 
-if (b == '\0')
+if (b == NULL)
 return (0);
 for (i = 0; b[i] != '\0'; i++)
 {
 if (b[i] != '0' && b[i] != '1')
 return (0);
-}
-for (i = 0; b[i] != '\0'; i++)
-{
+
 number <<= 1;
 if (b[i] == '1')
 number += 1;
