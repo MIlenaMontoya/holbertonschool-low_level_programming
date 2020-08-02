62d23a0ac013ba9b8f84d964f388e4c8bdc1e943
MIlenaMontoya <1852@holbertonschool.com>
2020-08-02 04:09:06 +0000

    bit manipulation

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -13,6 +13,7 @@ unsigned int number = 0;
 
 if (b == NULL)
 return (0);
+
 for (i = 0; b[i] != '\0'; i++)
 {
 if (b[i] != '0' && b[i] != '1')
---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+
+/**
+ * binary_to_uint - converts a binary number to an unsigned int.
+ * @b: pointer to a string
+ *
+ * Return: the converted number
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+unsigned int i;
+}
