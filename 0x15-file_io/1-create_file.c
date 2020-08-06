0792d85a40c60e568650eae4e88f00d42e5a2730
MIlenaMontoya <1852@holbertonschool.com>
2020-08-06 03:28:48 +0000

    file task one

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -15,12 +15,16 @@ int file_d, wr, len = 0;
 
 	if (filename == NULL)
 		return (-1);
-if (text_content != NULL)
 file_d = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	{
-		for (len = 0; text_content[len]; len++)
+if (file_d == -1)
+{
+return (-1);
+}
+if (text_content != NULL)
+{
+	for (len = 0; text_content[len]; len++)
 		;
-	}
+}
 	wr = write(file_d, text_content, len);
 	if (file_d == -1 || wr == -1)
 		return (-1);
