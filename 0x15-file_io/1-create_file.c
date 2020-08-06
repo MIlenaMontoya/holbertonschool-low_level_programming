fd6569cabbcd41e95486c8d3617d2a028c50b5c8
MIlenaMontoya <1852@holbertonschool.com>
2020-08-06 03:15:57 +0000

    file task one

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,15 +1,29 @@
 #include "holberton.h"
 
 /**
- * create_file - 
+ * create_file - creates a file
  * @filename: file to read and print
- * @text_content: number of letter
+ * @text_content: content text
  *
- * Return: number of letters
+ * Return: 0
  */
 
 int create_file(const char *filename, char *text_content)
 {
 
+int file_d, wr, len = 0;
 
+	if (filename == NULL)
+		return (-1);
+if (text_content != NULL)
+file_d = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	{
+		for (len = 0; text_content[len]; len++)
+		;
+	}
+	wr = write(file_d, text_content, len);
+	if (file_d == -1 || wr == -1)
+		return (-1);
+	close(file_d);
+	return (1);
 }
