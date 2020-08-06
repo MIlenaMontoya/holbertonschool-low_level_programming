c4326c15f86280edc8597954c3a6867a819ea5ee
MIlenaMontoya <1852@holbertonschool.com>
2020-08-06 04:24:11 +0000

    file task two

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,7 +1,7 @@
 #include "holberton.h"
 
 /**
- * append_text_to_file - reads a text file and prints it to the std output
+ * append_text_to_file - appends text at the end of a file
  * @filename: file to read and print
  * @text_content: number of letter
  *
@@ -10,5 +10,24 @@
 
 int append_text_to_file(const char *filename, char *text_content)
 {
+       int file_d, wr, len = 0;
 
+	if (filename == NULL)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		for (len = 0; text_content[len];)
+			len++;
+	}
+
+	file_d = open(filename, O_WRONLY | O_APPEND);
+	wr = write(file_d, text_content, len);
+
+	if (file_d == -1 || wr == -1)
+		return (-1);
+
+	close(file_d);
+
+	return (1);
 }
