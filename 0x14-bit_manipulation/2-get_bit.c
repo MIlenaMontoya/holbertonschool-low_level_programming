a15c7f423cc34c3195ec00e5482dcc044ba9e275
MIlenaMontoya <1852@holbertonschool.com>
2020-08-13 16:55:46 +0000

    task 2  get bit

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,12 +1,20 @@
 #include "holberton.h"
 
 /**
- * binary_to_uint - converts a binary number to an unsigned int.
- * @b: pointer to a string
+ * get_bit - returns the value of a bit at a given index.
+ * @n: integer number to find the binary index.
+ * @index: index of the binary to return.
  *
- * Return: the converted number
+ * Return: value the index or -1 if an error occured
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-unsigned int i;
+	unsigned long aux;
+
+	if (index >= (sizeof(n) * 8) - 1)
+	{
+		return (-1);
+	}
+	aux = n >> index;
+	return (aux & 1);
 }
