27655bf01d5ce1f54a6674ab87c99d12e97e996e
MIlenaMontoya <1852@holbertonschool.com>
2020-08-17 22:56:08 +0000

    betty

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -8,20 +8,20 @@
  */
 
 void rev_string(char *s)
+
 {
-int i;
-int index_b;
-int index_a;
+int i, a, b;
 char aux;
+
 for (i = 0; s[i] != 0; i++)
 {
 }
-index_b = i - 1;
-for (index_a = 0; index_a < index_b; index_a++)
+b = i - 1;
+for (a = 0; a < b; a++)
 {
-aux = s[index_a];
-s[index_a] = s[index_b];
-s[index_b] = aux;
-index_b--;
+aux = s[a];
+s[a] = s[b];
+s[b] = aux;
+b--;
 }
 }
