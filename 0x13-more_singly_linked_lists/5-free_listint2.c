38ee275f5c40834a4589a246690e2f28567e056c
MIlenaMontoya <1852@holbertonschool.com>
2020-09-06 00:08:47 +0000

    betty

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -7,14 +7,13 @@
  */
 void free_listint2(listint_t **head)
 {
-	listint_t *list_free;
-
-        if (head == NULL)
-        return;
+listint_t *list_free;
+if (head == NULL)
+return;
 for (; *head != NULL;)
-	{
-		list_free = (*head)->next;
-		free(*head);
-                *head = list_free;
-	}
+{
+ist_free = (*head)->next;
+free(*head);
+*head = list_free;
+}
 }
