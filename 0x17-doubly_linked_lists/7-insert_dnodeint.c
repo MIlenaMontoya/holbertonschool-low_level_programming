85682c938c6008d1fbbbc8dc3d83c18da2c651be
MIlenaMontoya <1852@holbertonschool.com>
2020-09-06 23:29:28 +0000

    not finish

---
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+
+/**
+ * insert_dnodeint_at_index -  function that inserts a new
+ * node at a given position.
+ * @h: double pointer
+ * @idx: index of the node
+ * @n: int
+ *
+ * Return: address of the new node
+ */
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+
+return (NULL);
+}
---
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+
+/**
+ * delete_dnodeint_at_index -  hat deletes the node at index
+ * of a dlistint_t linked list.
+ * @head: double pointer
+ * @index: index of the node
+ *
+ * Return: address of the new node
+ */
+
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+
+return (NULL);
+}
