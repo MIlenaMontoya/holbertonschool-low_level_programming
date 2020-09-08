bcbdf6ba2df6e09498bd5db6b7aaef423290cb17
MIlenaMontoya <1852@holbertonschool.com>
2020-09-08 01:13:14 +0000

    101 make

---
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+wget -P /tmp/jackpot.so https://github.com/MIlenaMontoya/holbertonschool-low_level_programming/tree/master/0x18-dynamic_libraries/jackpot.so
+export LD_PRELOAD=/tmp/winlot.so
